dba1d0d2a6ff1bded1e6161dddd6a3e5023659d1
leeh779 <leeh779@newschool.edu>
2019-11-05 15:22:25 -0500

    spaceX update

--- assignments/5/assets/script.js
+++ assignments/5/assets/script.js
@@ -27,6 +27,10 @@ fetch('https://api.spacexdata.com/v3/launches/latest')
 
 	console.log("results: ", jsonResults)
 
+	document.getElementById("launch-vid").innerHTML = `
+		<iframe class="launch-vid-child" width="640" height="480" src="https://www.youtube.com/embed/${jsonResults.links.youtube_id}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	`
+
 	document.getElementById("info-1").innerHTML = `
 	Flight_number: <span class="info-shake">${jsonResults.flight_number}</span>
 	`
@@ -39,9 +43,11 @@ fetch('https://api.spacexdata.com/v3/launches/latest')
 	Rocket_name: <span class="info-shake">${jsonResults.rocket.rocket_name}</span><br>
 	`
 
-	document.getElementById("info-4").innerHTML = `
-	Launch_success: <span class="info-shake"><a class="info-maintop-link" href="${jsonResults.links.video_link}">?????</a></span>
-	`
+	// document.getElementById("info-4").innerHTML = `
+	// Launch_success: <span class="info-shake"><a class="info-maintop-link" href="${jsonResults.links.video_link}">?????</a></span>
+	// `
+	// https://www.youtube.com/embed/fZh82-WcCuo
+	//<iframe class="launch-vid" width="640" height="480" src="https://www.youtube.com/embed/fZh82-WcCuo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 })
 .catch(function(error){
 	console.log("error message:", error)
